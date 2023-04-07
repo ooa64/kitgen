b76efae87e5ccfa0b2ff03fffcda53643d156627
ooa64 <ooa64@ua.fm>
2023-04-07 16:01:12 +0300

    added static yajl and tdjson

--- kitInit-custom.c
+++ kitInit-custom.c
@@ -1,2 +1,9 @@
+#if defined(KIT_INCLUDES_YAJLTCL)
     Tcl_AppInitProc Yajltcl_Init, Yajltcl_SafeInit;
     Tcl_StaticPackage(0, "yajltcl", Yajltcl_Init, Yajltcl_SafeInit);
+#endif
+
+#if defined(KIT_INCLUDES_TCLTDJSON)
+    Tcl_AppInitProc Tcltdjson_Init;
+    Tcl_StaticPackage(0, "tcltdjson", Tcltdjson_Init, NULL);
+#endif
--- setupvfs-custom.tcl
+++ setupvfs-custom.tcl
@@ -1,5 +1,15 @@
+if {"yajltcl" in $::env(CUSTOM)} {
     vfscopy lib/yajltcl1.7.0/yajl.tcl
     set fn $vfs/lib/yajltcl1.7.0/pkgIndex.tcl
     set f [open $fn w]
     puts $f "package ifneeded yajltcl 1.7.0 \"load {} Yajltcl; \[list source \[file join \$dir yajl.tcl]]\""
     close $f
+}
+
+if {"tcltdjson" in $::env(CUSTOM)} {
+    vfscopy lib/tcltdjson0.1/pkgIndex.tcl
+    set fn $vfs/lib/tcltdjson0.1/pkgIndex.tcl
+    set f [open $fn w]
+    puts $f "package ifneeded tcltdjson 0.1 \"load {} Tcltdjson\""
+    close $f
+}
